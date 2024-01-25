9ffb5afc7e9a54bdbadc2fd7268c0e24aac4dac8
snowkingS2 <155800727+snowkingS2@users.noreply.github.com>
2024-01-25 19:48:43 +0700

    Add files via upload

--- Tên Hub.lua
+++ Tên Hub.lua
@@ -3,8 +3,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 --------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "Tên Hub",
-    SubTitle = "by Tên Thằng Skid",
+    Title = "waifu hub",
+    SubTitle = "by DinosnowMC",
     TabWidth = 160,
     Size = UDim2.fromOffset(450, 300),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
